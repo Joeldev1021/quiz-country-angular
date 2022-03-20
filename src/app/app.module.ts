853ee541de334d27cdd11020f37cabf15153850d
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-03-20 12:10:43 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CountryCartComponent } from './components/country-cart/country-cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CountryCartComponent
   ],
   imports: [
     BrowserModule
