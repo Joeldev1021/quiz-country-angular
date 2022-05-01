1a1d62d110a98e3f9bc5da7a6ef21964e1cdd342
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-05-01 18:00:25 -0500

    feat:app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { AppComponent } from './app.component';
 import { CardComponent } from './components/card/card.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ListItemComponent } from './components/list-item/list-item.component';
+import { CardFinishComponent } from './components/card-finish/card-finish.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
     ListItemComponent,
+    CardFinishComponent
   ],
   imports: [
     BrowserModule,
